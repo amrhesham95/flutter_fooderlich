bbdc96a5956e412aa9735897aaf7466e3fb0a95c
amrhesham95 <47639999+amrhesham95@users.noreply.github.com>
2021-10-15 20:23:40 +0200

    Composing the text

--- lib/card2.dart
+++ lib/card2.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:fooderlich/author_card.dart';
-// import 'fooderlich_theme.dart';
+import 'fooderlich_theme.dart';
 
 class Card2 extends StatelessWidget {
   const Card2({Key? key}) : super(key: key);
@@ -19,8 +19,34 @@ class Card2 extends StatelessWidget {
         ),
         child: Column(
           children: [
-            AuthorCard(authorName: "Amr Hesham", title: "Senior iOS", imageProvider: AssetImage("assets/author_katz.jpeg"))
-            // TODO 4: add Positioned text
+            AuthorCard(authorName: "Amr Hesham", title: "Senior iOS", imageProvider: AssetImage("assets/author_katz.jpeg")),
+            // 1
+            Expanded(
+// 2
+              child: Stack(
+                children: [
+// 3
+                  Positioned(
+                    bottom: 16,
+                    right: 16,
+                    child: Text(
+                      'Recipe',
+                      style: FooderlichTheme.lightTextTheme.headline1,
+                    ),
+                  ),
+// 4
+                  Positioned(
+                    bottom: 70,
+                    left: 16,
+                    child: RotatedBox(
+                      quarterTurns: 3,
+                      child: Text(
+                        'Smoothies',
+                        style: FooderlichTheme.lightTextTheme.headline1,
+                      ),
+                    ), ),
+                ], ),
+            ),
           ],
         ),
       ),
